4e25ab3c13ae020f72a1629756b59b1b83b17518
Rakesh-koneti <rakeshkoneti123@gmail.com>
2025-05-08 22:21:58 +0530

    Updated routes configuration to properly handle public and protected routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,4 +1,5 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
+import { AuthProvider } from './lib/auth-context';
 import { HomePage } from './pages/home';
 import { AboutPage } from './pages/about';
 import { ContactPage } from './pages/contact';
@@ -9,20 +10,53 @@ import { StreamExamsPage } from './pages/exams/stream-exams';
 import { ExamPage } from './pages/exam';
 import { ExamResultPage } from './pages/exam-result';
 import { ExamReviewPage } from './pages/exam-review';
+import { IPEWeightagePage } from './pages/ipe-weightage';
 
 export function AppRoutes() {
   return (
     <Routes>
+      {/* Public Routes */}
       <Route path="/" element={<HomePage />} />
       <Route path="/about" element={<AboutPage />} />
       <Route path="/contact" element={<ContactPage />} />
-      <Route path="/mock-tests" element={<MockTestsPage />} />
-      <Route path="/previous-year-papers" element={<PreviousYearPapersPage />} />
-      <Route path="/exams" element={<ExamsPage />} />
-      <Route path="/exams/:stream" element={<StreamExamsPage />} />
-      <Route path="/exam/:examId" element={<ExamPage />} />
-      <Route path="/exam/:examId/result" element={<ExamResultPage />} />
-      <Route path="/exam/:examId/review" element={<ExamReviewPage />} />
+      <Route path="/ipe-weightage" element={<IPEWeightagePage />} />
+
+      {/* Protected Routes */}
+      <Route path="/mock-tests" element={
+        <AuthProvider>
+          <MockTestsPage />
+        </AuthProvider>
+      } />
+      <Route path="/previous-year-papers" element={
+        <AuthProvider>
+          <PreviousYearPapersPage />
+        </AuthProvider>
+      } />
+      <Route path="/exams" element={
+        <AuthProvider>
+          <ExamsPage />
+        </AuthProvider>
+      } />
+      <Route path="/exams/:stream" element={
+        <AuthProvider>
+          <StreamExamsPage />
+        </AuthProvider>
+      } />
+      <Route path="/exam/:examId" element={
+        <AuthProvider>
+          <ExamPage />
+        </AuthProvider>
+      } />
+      <Route path="/exam/:examId/result" element={
+        <AuthProvider>
+          <ExamResultPage />
+        </AuthProvider>
+      } />
+      <Route path="/exam/:examId/review" element={
+        <AuthProvider>
+          <ExamReviewPage />
+        </AuthProvider>
+      } />
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
